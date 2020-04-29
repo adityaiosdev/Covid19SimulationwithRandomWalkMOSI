e4ee9826960ff0b44da2d9e746a65e1e1bef8fe9
beginner75 <44922464+adhityahardy@users.noreply.github.com>
2020-04-29 19:56:32 +0700

    update jalan

--- p5/framework/sketch.js
+++ p5/framework/sketch.js
@@ -1,7 +1,59 @@
+let bola=[];
+let jumlahbola=100;
+let diam=10;
+
 function setup() {
-  // put setup code here
+  createCanvas(1000,700);
+  background(0);
+  for (var i = 0; i < jumlahbola; i++) {
+    bola[i] = new Balls(
+      i, //ID number
+      random(width), //start x coord
+      random(height), //start y coord
+      random(-8, 8), //start x velocity
+      random(-8, 8), //start y velocity
+      bola
+    );
+  }
+  frameRate(120); 
 }
 
 function draw() {
-  // put drawing code here
+  background(100);
+  for (i = 0; i < jumlahbola; i++) {
+        bola[i].move();
+        bola[i].display();
+      }
+    }
+
+
+class Balls {
+  constructor(
+    id = 0,
+    x,
+    y,
+    vx = random(-8, 8),
+    vy = random(-8, 8),
+    others= []) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.others = others;
+  }
+  move() {
+      if (this.x < 0 || this.x > width) {
+        this.vx = -1 * this.vx;
+      }
+      if (this.y < 0 || this.y > height) {
+        this.vy = -1 * this.vy;
+      }
+      this.x += this.vx;
+      this.y += this.vy;
+  }
+  display() {
+    fill(0);
+    ellipse(this.x, this.y, 10, 10);
+  }
 }
